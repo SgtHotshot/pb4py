eea17a5f95b76486a15c1d4a37be25827adea7b1
James Durand <james@nstargaming.com>
2014-08-29 09:45:47 -0500

    Add myself to contributors

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
 	name             = 'PyBullet',
 	version          = '1.0.0.0',
 	description      = 'Python API for PushBullet',
-	author           = 'sgthotshot',
+	author           = ['sgthotshot', 'cadyyan'],
 	url              = 'https://www.sgthotshot.com/pybullet',
 	packages         = find_packages(),
 	install_requires = [
