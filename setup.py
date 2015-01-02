1a22ffabc19e542474b11261e7b4fd4efae8b520
Sgthotshot <sgt.hotshot@gmail.com>
2015-01-02 16:18:25 -0600

    Setting up PyPi

--- setup.py
+++ setup.py
@@ -6,7 +6,10 @@ setup(
 	description      = 'Python API for PushBullet',
 	author           = ['sgthotshot', 'cadyyan'],
 	url              = 'https://www.sgthotshot.com/pb4py',
+	download_url	 = 'https://github.com/SgtHotshot/pb4py/tarball/1.1',
 	packages         = find_packages(),
+	keywords		 = ['PushBullet', 'notifications', 'messaging'],
+	classifiers		 = [],
 	install_requires = [
 		'requests',
 	],
