6887aa29f258c8942b38731b58ad8542364dde30
Cadyyan <cadyyan@gmail.com>
2015-08-24 12:49:24 +0000

    Add longer description to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as readme:
+	long_description = readme.read()
+
 setup(
 	name             = 'pb4py',
 	version          = '1.0.0.0',
 	description      = 'Python API for PushBullet',
+	long_description = long_description,
 	author           = ['sgthotshot', 'cadyyan'],
 	url              = 'https://www.sgthotshot.com/pb4py',
 	download_url	 = 'https://github.com/SgtHotshot/pb4py/tarball/1.1',
