f85acf2d4e09c8872b64c650467cd6975edf43ec
James Durand <james.durand@alumni.msoe.edu>
2015-08-24 16:13:54 +0000

    Add the rest of the device management commands

--- scripts/pb4.py
+++ scripts/pb4.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 
 import argparse
 import json
+import sys
 import tabulate
 
 import pb4py
@@ -17,6 +18,72 @@ def add_device_commands(parsers):
 		'list',
 		help = 'List all devices.',
 	).set_defaults(func = command_devices_list)
+
+
+
+	create_device = device_parsers.add_parser(
+		'create',
+		help = 'Create a new PushBullet device.',
+	)
+
+	create_device.add_argument(
+		'name',
+		type = str,
+		help = 'The name of the device',
+	)
+
+	create_device.add_argument(
+		'device_type',
+		type = str,
+		help = 'The type of device (ex: Windows, phone, etc)',
+	)
+
+	create_device.set_defaults(func = command_devices_create)
+
+
+
+	update_device = device_parsers.add_parser(
+		'update',
+		help = 'Update an existing PushBullet device.',
+	)
+
+	update_device.add_argument(
+		'iden',
+		type = str,
+		help = 'The ID of the device',
+	)
+
+	update_device.add_argument(
+		'--nickname',
+		type    = str,
+		default = None,
+		help    = 'Set the device nickname',
+	)
+
+	update_device.add_argument(
+		'--device_type',
+		dest    = 'type',
+		type    = str,
+		default = None,
+		help    = 'Set the device type',
+	)
+
+	update_device.set_defaults(func = command_devices_update)
+
+
+
+	delete_device = device_parsers.add_parser(
+		'delete',
+		help = 'Delete an existing PushBullet device.',
+	)
+
+	delete_device.add_argument(
+		'iden',
+		type = str,
+		help = 'The ID of the device',
+	)
+
+	delete_device.set_defaults(func = command_devices_delete)
 
 def add_parser_commands(parsers):
 	parsers.add_parser(
@@ -40,6 +107,38 @@ def parse_args():
 	return parser.parse_args()
 
 
+
+def prompt(question, default = "yes"):
+	"""
+	Ask a yes/no question via raw_input() and return their answer.
+
+	"question" is a string that is presented to the user.
+	"default" is the presumed answer if the user just hits <Enter>.
+		It must be "yes" (the default), "no" or None (meaning
+		an answer is required of the user).
+
+	The "answer" return value is True for "yes" or False for "no".
+	"""
+
+	valid = {'yes': True, 'y': True, 'ye': True, 'no': False, 'n': False}
+	if default is None:
+		options = ' [y/n] '
+	elif default == 'yes':
+		options = ' [Y/n] '
+	elif default == 'no':
+		options = ' [y/N] '
+	else:
+		raise ValueError('invalid default answer: \'%s\'' % default)
+
+	while True:
+		sys.stdout.write(question + options)
+		choice = raw_input().lower()
+		if default is not None and choice == '':
+			return valid[default]
+		elif choice in valid:
+			return valid[choice]
+		else:
+			sys.stdout.write('Please respond with \'yes\' or \'no\' (or \'y\' or \'n\').\n')
 
 def build_table(data, columns):
 	return tabulate.tabulate(
@@ -74,7 +173,36 @@ def command_generate_default_config(args):
 def command_devices_list(client, args):
 	devices = client.devices()
 
-	print(build_table(devices, ['nickname', 'type']))
+	print(build_table(devices, ['iden', 'nickname', 'type']))
+
+@client_command
+def command_devices_create(client, args):
+	client.create_device(args.name, args.device_type)
+
+	print('Device created')
+
+@client_command
+def command_devices_update(client, args):
+	updates = {
+		k: getattr(args, k)
+		for k in ['nickname', 'type']
+		if getattr(args, k) is not None
+	}
+
+	client.update_device(args.iden, updates)
+
+	print('Device updated')
+
+@client_command
+def command_devices_delete(client, args):
+	device = args.iden
+	confirm = prompt('Are you sure you want to delete device {}?'.format(device), default = 'no')
+	if not confirm:
+		return
+
+	client.delete_device(device)
+
+	print('Device deleted')
 
 
 
